7461e4fa0cb6a61e7cb27114dfee59c66e084445
GeorgianaAida <aida.georgiana@yahoo.com>
2025-03-26 18:09:49 +0200

    Am actualizat proiectul

---
+++ src/test/java/pages/WebTablePage.java
@@ -0,0 +1,83 @@
+package pages;
+
+import helpMethods.ElementHelper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import pageLocators.WebTableLocators;
+
+import java.util.List;
+
+import static pageLocators.PracticeFormLocators.tablesRowsListElement;
+import static pageLocators.WebTableLocators.*;
+import static pageLocators.WebTableLocators.*;
+
+public class WebTablePage {
+
+    private WebDriver driver;
+    private ElementHelper elementHelper;
+
+    public WebTablePage(WebDriver driver) {
+        this.driver = driver;
+        elementHelper = new ElementHelper(driver);
+    }
+
+    public void verifyTableContent(int expectedTablesRowListElement){
+        elementHelper.waitForElementsVisible(tablesRowListElement);
+        List<WebElement> tablesRowsList = driver.findElements(tablesRowListElement);
+        Assert.assertEquals(tablesRowsList.size(), expectedTablesRowListElement, "Valoarea initiala a tabelului nu este 3");
+    }
+
+    public void addNewRecordButtonClick(){
+        elementHelper.clickJSLocator(addElement);
+    }
+
+    public void fillFirstName(String firstNameValue){
+        elementHelper.fillLocator(firstNameElement, firstNameValue);
+    }
+
+    public void filllastName(String lastNameValue){
+        elementHelper.fillLocator(lastNameElement, lastNameValue);
+    }
+
+    public void fillEmail(String userEmailValue){
+        elementHelper.fillLocator(userEmailElement, userEmailValue);
+    }
+
+    public void fillAge(String ageValue){
+        elementHelper.fillLocator(ageElement, ageValue);
+    }
+
+    public void fillSalary(String salaryValue){
+        elementHelper.fillLocator(salaryElement, salaryValue);
+    }
+
+    public void fillDepartment(String departmentValue){
+        elementHelper.fillLocator(departmentElement, departmentValue);
+    }
+
+    public void submitClick(){
+        elementHelper.clickJSLocator(submitElement);
+    }
+
+    public void verifyTableContent(int expectedSize, String firstNameValue, String lastNameValue, String userEmailValue,
+                                   String ageValue, String salaryValue, String departmentValue){
+        List<WebElement> tablesRowsList = driver.findElements(tablesRowListElement);
+        Assert.assertEquals(tablesRowsList.size(), expectedSize, "Valoarea actuala a tabelului nu este 4");
+        elementHelper.validateTextContainsElement(tablesRowsList.get(3),firstNameValue);
+        elementHelper.validateTextContainsElement(tablesRowsList.get(3),lastNameValue);
+        elementHelper.validateTextContainsElement(tablesRowsList.get(3),userEmailValue);
+        elementHelper.validateTextContainsElement(tablesRowsList.get(3),ageValue);
+        elementHelper.validateTextContainsElement(tablesRowsList.get(3),salaryValue);
+        elementHelper.validateTextContainsElement(tablesRowsList.get(3),departmentValue);
+    }
+
+    public void deleteRecords(){
+        elementHelper.clickJSLocator(deleteElement);
+    }
+
+    public void editClick(){
+        elementHelper.clickJSLocator(editElement);
+    }
+}
--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -7,7 +7,6 @@ import org.testng.annotations.Test;
 import pages.IndexPage;
 import pages.PracticeFormPage;
 import shareData.ShareData;
-import shareData.ShareData;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -24,10 +23,10 @@ public class PracticeFormTest extends ShareData {
         indexPage.enterOnPracticeFormSubMenu();
 
         String firstNameValue = "Aida";
-        String lastNameValue = "Alexandroniu";
-        String emailValue = "aida.georgiana@gmail.com";
+        String lastNameValue = "Georgiana";
+        String emailValue = "aida.georgiana.com";
         String genderValue = "Female";
-        String mobilePhoneValue = "0711223344";
+        String mobilePhoneValue = "074555555";
         List<String> subjects = new ArrayList<>();
         subjects.add("Maths");
         subjects.add("Arts");
@@ -37,9 +36,11 @@ public class PracticeFormTest extends ShareData {
         hobbies.add("Sports");
         hobbies.add("Reading");
         hobbies.add("Music");
-        String addressValue = "Dumbravita";
+        String addressValue = "Timisoara";
         String pathValue = "src/test/resources/Screenshot 2024-10-28 200509.png";
-
+        String stateInputValue = "NCR";
+        String cityInputValue = "Delhi";
+        String expectedMessage = "Thanks for submitting the form";
 
         PracticeFormPage practiceFormPage = new PracticeFormPage(driver);
         practiceFormPage.fillFirstName(firstNameValue);
@@ -51,78 +52,10 @@ public class PracticeFormTest extends ShareData {
         practiceFormPage.fillHobbies(hobbies);
         practiceFormPage.pictureUpload(pathValue);
         practiceFormPage.fillCurrentAddress(addressValue);
-
-
-//        By stateElement = By.xpath("//div[text()='Select State']");
-//        elementHelper.clickJSLocator(stateElement);
-//
-//        By stateInputElement = By.id("react-select-3-input");
-//        String stateInputValue = "NCR";
-//        elementHelper.fillPressLocator(stateInputElement,stateInputValue,Keys.ENTER);
-//
-//        By cityElement = By.xpath("//div[text()='Select City']");
-//        elementHelper.clickJSLocator(cityElement);
-//
-//        By cityInputElement = By.id("react-select-4-input");
-//        String cityInputValue = "Delhi";
-//        elementHelper.fillPressLocator(cityInputElement,cityInputValue,Keys.ENTER);
-//
-//
-//        By submitElement = By.id("submit");
-//        elementHelper.clickJSLocator(submitElement);
-//
-//        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("example-modal-sizes-title-lg")));
-//
-//        By thankYouElement = By.id("example-modal-sizes-title-lg");
-//        String expectedMessage = "Thanks for submitting the form";
-//        elementHelper.validateTextLocator(thankYouElement,expectedMessage);
-////        String actualMessage = thankYouElement.getText();
-////        Assert.assertEquals(actualMessage, expectedMessage);
-////        System.out.println("Testul a fost validat!");
-//
-//       By rowListElement = By.xpath("//tbody/tr");
-//
-//        List<WebElement> rowList = driver.findElements(rowListElement);
-//        elementHelper.validateTextContainsElement(rowList.get(0),("Student Name"));
-//        elementHelper.validateTextContainsElement(rowList.get(0),(firstNameValue));
-//        elementHelper.validateTextContainsElement(rowList.get(0),(lastNameValue));
-//
-//        elementHelper.validateTextContainsElement(rowList.get(1),("Student Email"));
-//        elementHelper.validateTextContainsElement(rowList.get(1),(emailValue));
-//
-//        elementHelper.validateTextContainsElement(rowList.get(2),("Gender"));
-//        elementHelper.validateTextContainsElement(rowList.get(2),(genderValue));
-//
-//        elementHelper.validateTextContainsElement(rowList.get(3),("Mobile"));
-//        elementHelper.validateTextContainsElement(rowList.get(3),(phoneValue));
-//
-//
-//        String subjectsStringValue=String.join(", ",subjects);
-//        elementHelper.validateTextContainsElement(rowList.get(5),("Subjects"));
-//        System.out.println(rowList.get(5).getText());
-//        System.out.println(subjectsStringValue);
-//        elementHelper.validateTextContainsElement(rowList.get(5),(subjectsStringValue));
-//
-//        String hobbiesStringValue=String.join(", ",hobbies);
-//        elementHelper.validateTextContainsElement(rowList.get(6),("Hobbies"));
-//        elementHelper.validateTextContainsElement(rowList.get(6),(hobbiesStringValue));
-//
-////        elementHelper.validateTextContainsElement(rowList.get(7),("Picture"));
-////        elementHelper.validateTextContainsElement(rowList.get(7),("src/test/resources/Screenshot 2024-10-28 200509.png"));
-//
-//        elementHelper.validateTextContainsElement(rowList.get(8),("Address"));
-//        elementHelper.validateTextContainsElement(rowList.get(8),(currentAddressValue));
-//
-//
-//
-////        List<WebElement> RowList =driver.findElements(By.xpath("//tbody/tr"));
-//        elementHelper.validateTextContainsElement(rowList.get(9),("State and City"));
-//        elementHelper.validateTextContainsElement(rowList.get(9),(stateInputValue));
-//        elementHelper.validateTextContainsElement(rowList.get(9),(cityInputValue));
-//
-//
-//
-//
+        practiceFormPage.fillState(stateInputValue);
+        practiceFormPage.fillCity(cityInputValue);
+        practiceFormPage.submitClick();
+        practiceFormPage.validateThankYouMessage(expectedMessage);
+        practiceFormPage.validateEntireTable(firstNameValue, lastNameValue, emailValue, genderValue, mobilePhoneValue, addressValue, stateInputValue, cityInputValue);
     }
 }
