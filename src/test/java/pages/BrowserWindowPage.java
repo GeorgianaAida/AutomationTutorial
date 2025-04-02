0d3a93cc9b1197690a55527f1688cfdead9a39bb
GeorgianaAida <aida.georgiana@yahoo.com>
2025-04-02 19:07:21 +0300

    Am adaugat Logger

--- src/test/java/pages/BrowserWindowPage.java
+++ src/test/java/pages/BrowserWindowPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import helpMethods.ElementHelper;
 import helpMethods.TabWindowHelper;
+import loggerUtility.LoggerUtility;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -22,20 +23,26 @@ public class BrowserWindowPage {
 
     public void interactWithSpecificTab() {
         elementHelper.clickJSLocator(tabButtonElement);
+        LoggerUtility.infoTest("The user clicks on Tab Button");
 
         System.out.println(driver.getCurrentUrl());
         tabWindowHelper.switchToSpecificTabWindow(1);
+        LoggerUtility.infoTest("The user switches the focus on second tab");
 
         System.out.println(driver.getCurrentUrl());
 
         tabWindowHelper.closeCurrentTabWindow();
+        LoggerUtility.infoTest("The user close the second tab");
     }
 
     public void interactWithSpecificWindow() {
         tabWindowHelper.switchToSpecificTabWindow(0);
+        LoggerUtility.infoTest("The user switches the focus on second window");
 
         elementHelper.clickJSLocator(windowButtonElement);
+        LoggerUtility.infoTest("The user switches the focus on second window");
 
         tabWindowHelper.switchToSpecificTabWindow(1);
+        LoggerUtility.infoTest("The user close the second window");
     }
 }
--- src/test/java/shareData/ShareData.java
+++ src/test/java/shareData/ShareData.java
@@ -1,29 +1,31 @@
 package shareData;
 
+import loggerUtility.LoggerUtility;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.time.Duration;
-
 public class ShareData {
-    //Aceasta clasa are ca rol sa deschida/inchida un browser
-    //Vom face acest lucru folosind adnotari de TestNG
 
     public WebDriver driver;
+    public String testName;
 
     @BeforeMethod
-    public void prepareEnvironment() {
+    public void prepareEnvironment(){
         driver = new ChromeDriver();
         driver.get("https://demoqa.com");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        testName = this.getClass().getSimpleName();
+        LoggerUtility.startTest(testName);
     }
 
-
     @AfterMethod
-    public void clearEnvironment() {
+    public void clearEnvironment(){
         driver.quit();
+
+        testName = this.getClass().getSimpleName();
+        LoggerUtility.finishTest(testName);
     }
 }
